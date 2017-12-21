c7682a4665a0c2fb9a87feed90b67bbd7c068781
Elli Scharlin <escharlin@wesleyan.edu>
2017-12-21 00:50:44 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,7 @@
 def position_taken?(array, index)
   if array[index] == " " || array[index] == nil || array[index] == "" 
     return false
-
+  elsif array[index] == "X" || array[index] == "O"
+    return true
+  end
 end
